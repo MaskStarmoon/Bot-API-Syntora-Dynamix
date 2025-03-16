e36e704b7cfa1c7171b61912e0d5e0e16f58fca7
MaskStarmoon <150095487+MaskStarmoon@users.noreply.github.com>
2025-03-16 03:36:20 -0700

    Create nya.js

---
+++ cmd_module/cmd/nya.js
@@ -0,0 +1,12 @@
+module.exports = {
+  config: { 
+    nama: "nya", 
+    penulis: "Range", 
+    kuldown: 10,
+    peran: 0,
+    tutor: "balas nyaw."
+  }, 
+  Alya: async function (args) {
+    return "Jangan ngespam yaa...^^";
+  }
+};
