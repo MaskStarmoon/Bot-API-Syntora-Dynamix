baa32ab354f367ea180ab8b383fec8ae35524983
MaskStarmoon <150095487+MaskStarmoon@users.noreply.github.com>
2025-03-16 03:20:24 -0700

    Update server-api.js

--- server-api.js
+++ server-api.js
@@ -1,1 +1,7 @@
-// api code
+// server api code
+const express = require('express');
+const app = express();
+const fs = require('fs');
+const axios = require('axios');
+const path = require('path');
+const { cmd_src } = require('./cmd_module/index.js');
