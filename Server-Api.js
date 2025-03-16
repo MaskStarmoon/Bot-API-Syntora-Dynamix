e74ed5963987e8d09aa58a0a632c37073c355a0f
MaskStarmoon <150095487+MaskStarmoon@users.noreply.github.com>
2025-03-16 04:58:50 -0700

    Update Server-Api.js

--- Server-Api.js
+++ Server-Api.js
@@ -39,6 +39,4 @@ app.get('/api/command', async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+app.listen(PORT, () => {});
