c4d4cc5412e2dbffd5c046c324007497b76548e1
MaskStarmoon <150095487+MaskStarmoon@users.noreply.github.com>
2025-03-16 03:34:10 -0700

    Update index.js

--- cmd_module/index.js
+++ cmd_module/index.js
@@ -1,1 +1,27 @@
-// code module cmd
+// code load module cmd
+const fs = require('fs');
+const path = require('path');
+
+const folderCmdPath = path.join(__dirname, 'cmd_module', 'cmd');
+const cmd_src = {};
+
+fs.readdirSync(folderCmdPath).forEach(file => {
+    if (file.endsWith('.js')) {
+        const filePath = path.join(folderCmdPath, file);
+        try {
+            const command = require(filePath);
+            if (command.config && command.config.nama) {
+                cmd_src[command.config.nama] = {
+                    config: command.config,
+                    Syntora: async function (...args) {
+                        return command.Syntora ? await command.Syntora(...args) : null;
+                    }
+                };
+            }
+        } catch (error) {
+            console.error(`Gagal meload file ${file}:`, error);
+        }
+    }
+});
+
+module.exports = { cmd_src };
